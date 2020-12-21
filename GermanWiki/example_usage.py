db85e50531102feb37595b3ece6df8d12bd6a544
ankitrjk <67579096+ankitrjk@users.noreply.github.com>
2020-12-21 18:16:49 +0530

    Update example_usage.py

--- GermanWiki/example_usage.py
+++ GermanWiki/example_usage.py
@@ -4,35 +4,35 @@ from googletrans import Translator
 import pandas as pd
 
 
-# # parse the dump
-# # german_wiki_module.parse_dump(
-# #     'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.xml')
+# parse the dump
+german_wiki_module.parse_dump(
+    'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.xml')
 
-# # get number of revisions for an article
-# print(german_wiki_module.get_number_of_revisions('Luftstreitkräfte der Sowjetunion',
-#                                                  'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
+# get number of revisions for an article
+print(german_wiki_module.get_number_of_revisions('Luftstreitkräfte der Sowjetunion',
+                                                 'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
 
-# # get number of unique editors for an article
-# print(german_wiki_module.get_number_of_unique_editors('Luftstreitkräfte der Sowjetunion',
-#                                                       'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
+# get number of unique editors for an article
+print(german_wiki_module.get_number_of_unique_editors('Luftstreitkräfte der Sowjetunion',
+                                                      'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
 
-# # get total number of bytes edited for every revision of an article
-# print(german_wiki_module.get_number_of_bytes_edited('Luftstreitkräfte der Sowjetunion',
-#                                                     'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
-
-
-# # get list of all the articles present in a category
-# print(german_wiki_module.get_all_articles_in_a_category(
-#     'Luftstreitkräfte der Sowjetunion'))
+# get total number of bytes edited for every revision of an article
+print(german_wiki_module.get_number_of_bytes_edited('Luftstreitkräfte der Sowjetunion',
+                                                    'dewiki-20201020-stub-meta-history.xml/dewiki-20201020-stub-meta-history.csv'))
 
 
-# # get readability score of an article
-# print(german_wiki_module.get_readability_score(
-#     'Luftstreitkräfte der Sowjetunion'))
+# get list of all the articles present in a category
+print(german_wiki_module.get_all_articles_in_a_category(
+    'Luftstreitkräfte der Sowjetunion'))
 
-# # get number of pageviews of an article
-# print(german_wiki_module.get_number_of_pageviews(
-#     'Luftstreitkräfte der Sowjetunion'))
+
+# get readability score of an article
+print(german_wiki_module.get_readability_score(
+    'Luftstreitkräfte der Sowjetunion'))
+
+# get number of pageviews of an article
+print(german_wiki_module.get_number_of_pageviews(
+    'Luftstreitkräfte der Sowjetunion'))
 
 
 # get list of  the all articles present in a category
