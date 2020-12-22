2e2983b9ddfcafcd47a2d9f1dc0b692b6bf7766f
Akash Rawat <61121894+akashrawat2405@users.noreply.github.com>
2020-12-22 19:43:09 +0530

    Delete parse_dump.py

--- EnglishWiki/parse_dump.py
+++
@@ -1,6 +0,0 @@
-import wiki_dump_parser as parser
-
-
-# this function reads the XML dump of wikipedia and outputs the processed data in a CSV file
-def parse_dump(file_path):
-    parser.xml_to_csv(file_path)
