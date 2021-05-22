0eed79449e49baa5a1276d18dfff2f164156098d
Akash Rawat <61121894+akashrawat2405@users.noreply.github.com>
2021-05-22 23:18:33 +0530

    Add files via upload

---
+++ editors data 2020/editors_by_country/fr.wikipedia/editors with 5 - 99 edits/count.py
@@ -0,0 +1,51 @@
+import json 
+import csv
+  
+# Opening JSON file 
+de = 0
+others = 0
+de_list =["France"]
+others_list = ["Others"]
+
+file_= open('data.csv', 'w')
+
+with file_:
+      writer = csv.writer(file_)
+
+      for i in range(1,13):
+            filename = str(i) + ".json"
+            print(filename)
+            f = open(filename,) 
+
+            # returns JSON object as  
+            # a dictionary 
+            data = json.load(f) 
+            
+            # Iterating through the json 
+            # list 
+            temp = 0
+            for item in data['items']: 
+                  for countries in item['countries']:
+                        if(countries['country']=="FR"):
+                              de_list.append(countries['editors-ceil'])
+                              de = de + countries['editors-ceil']
+                        else:
+                              temp = temp + countries['editors-ceil']
+                              others = others + countries['editors-ceil']
+            
+            others_list.append(temp)
+
+            # print(de)
+            # print(others)
+            # Closing file 
+
+            f.close() 
+
+      de_list.append(de)
+      others_list.append(others)
+      
+      writer.writerow(["Country","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","TOTAL"])
+
+      writer.writerow(de_list)
+      writer.writerow(others_list)
+      file_.close
